f1238763b147eeb26440ee0148294d67a38f3aec
wujun <wujun728@163.com>
2023-07-06 17:58:48 +0800

    调整代码生成器

---
+++ src/main/java/com/gitthub/wujun728/engine/common/BaseEntity.java
@@ -0,0 +1,40 @@
+package com.gitthub.wujun728.engine.common;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * BaseEntity
+ *
+ * @author wujun
+ * @version V1.0
+ * @date 2020年3月18日
+ */
+@Data
+public class BaseEntity {
+	// -------------------------------------分页信息----------------------------------------------
+    @JSONField(serialize = false)
+    @TableField(exist = false)
+    private int page = 1;
+
+    @JSONField(serialize = false)
+    @TableField(exist = false)
+    private int limit = 10;
+    
+    @JSONField(serialize = false)
+    @TableField(exist = false)
+    private String keyword;
+
+    // -------------------------------------数据权限----------------------------------------------
+    /**
+     * 数据权限：用户id
+     */
+    @TableField(exist = false)
+    private List<String> createIds;
+    
+	@TableField(exist = false)
+	private Integer isOwner;
+}
