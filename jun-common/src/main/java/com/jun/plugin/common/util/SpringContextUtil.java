364d41f370fda5946715b85e27b9de29998f75cf
wujun <wujun728@163.com>
2023-12-14 17:57:19 +0800

    1

--- jun-common/src/main/java/com/jun/plugin/common/util/SpringContextUtil.java
+++ jun-common/src/main/java/com/jun/plugin/common/util/SpringContextUtil.java
@@ -14,7 +14,129 @@ import java.util.Arrays;
 import java.util.Map;
 
 @Component
-public class SpringContextUtil implements ApplicationContextAware {
+public class SpringContextUtil  implements BeanFactoryPostProcessor, ApplicationContextAware {
+    private static ConfigurableListableBeanFactory beanFactory;
+    private static ApplicationContext applicationContext;
+
+    public SpringUtil() {
+    }
+
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        SpringUtil.beanFactory = beanFactory;
+    }
+
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        SpringUtil.applicationContext = applicationContext;
+    }
+
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    public static ListableBeanFactory getBeanFactory() {
+        ListableBeanFactory factory = null == beanFactory ? applicationContext : beanFactory;
+        if (null == factory) {
+            throw new UtilException("No ConfigurableListableBeanFactory or ApplicationContext injected, maybe not in the Spring environment?");
+        } else {
+            return (ListableBeanFactory)factory;
+        }
+    }
+
+    public static ConfigurableListableBeanFactory getConfigurableBeanFactory() throws UtilException {
+        ConfigurableListableBeanFactory factory;
+        if (null != beanFactory) {
+            factory = beanFactory;
+        } else {
+            if (!(applicationContext instanceof ConfigurableApplicationContext)) {
+                throw new UtilException("No ConfigurableListableBeanFactory from context!");
+            }
+
+            factory = ((ConfigurableApplicationContext)applicationContext).getBeanFactory();
+        }
+
+        return factory;
+    }
+
+    public static <T> T getBean(String name) {
+        return getBeanFactory().getBean(name);
+    }
+
+    public static <T> T getBean(Class<T> clazz) {
+        return getBeanFactory().getBean(clazz);
+    }
+
+    public static <T> T getBean(String name, Class<T> clazz) {
+        return getBeanFactory().getBean(name, clazz);
+    }
+
+    public static <T> T getBean(TypeReference<T> reference) {
+        ParameterizedType parameterizedType = (ParameterizedType)reference.getType();
+        Class<T> rawType = (Class)parameterizedType.getRawType();
+        Class<?>[] genericTypes = (Class[])Arrays.stream(parameterizedType.getActualTypeArguments()).map((type) -> {
+            return (Class)type;
+        }).toArray((x$0) -> {
+            return new Class[x$0];
+        });
+        String[] beanNames = getBeanFactory().getBeanNamesForType(ResolvableType.forClassWithGenerics(rawType, genericTypes));
+        return getBean(beanNames[0], rawType);
+    }
+
+    public static <T> Map<String, T> getBeansOfType(Class<T> type) {
+        return getBeanFactory().getBeansOfType(type);
+    }
+
+    public static String[] getBeanNamesForType(Class<?> type) {
+        return getBeanFactory().getBeanNamesForType(type);
+    }
+
+    public static String getProperty(String key) {
+        return null == applicationContext ? null : applicationContext.getEnvironment().getProperty(key);
+    }
+
+    public static String getApplicationName() {
+        return getProperty("spring.application.name");
+    }
+
+    public static String[] getActiveProfiles() {
+        return null == applicationContext ? null : applicationContext.getEnvironment().getActiveProfiles();
+    }
+
+    public static String getActiveProfile() {
+        String[] activeProfiles = getActiveProfiles();
+        return ArrayUtil.isNotEmpty(activeProfiles) ? activeProfiles[0] : null;
+    }
+
+    public static <T> void registerBean(String beanName, T bean) {
+        ConfigurableListableBeanFactory factory = getConfigurableBeanFactory();
+        factory.autowireBean(bean);
+        factory.registerSingleton(beanName, bean);
+    }
+
+    public static void unregisterBean(String beanName) {
+        ConfigurableListableBeanFactory factory = getConfigurableBeanFactory();
+        if (factory instanceof DefaultSingletonBeanRegistry) {
+            DefaultSingletonBeanRegistry registry = (DefaultSingletonBeanRegistry)factory;
+            registry.destroySingleton(beanName);
+        } else {
+            throw new UtilException("Can not unregister bean, the factory is not a DefaultSingletonBeanRegistry!");
+        }
+    }
+
+    public static void publishEvent(ApplicationEvent event) {
+        if (null != applicationContext) {
+            applicationContext.publishEvent(event);
+        }
+
+    }
+
+    public static void publishEvent(Object event) {
+        if (null != applicationContext) {
+            applicationContext.publishEvent(event);
+        }
+
+    }
+ 
+
 
     private static ApplicationContext applicationContext;
 
